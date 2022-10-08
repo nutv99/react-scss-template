b68ced526e4bac3a7128d3a48c56f1ca4882e7d6
nutv99 <nutv99@gmail.com>
2022-10-08 12:16:24 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-scss-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mpimra)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>ssss</h1>
+      <div className="box">5555</div>
+      <div className="box">66665555</div>
+      <div className="flexRow">
+        <div className="card boxShadow">Card1</div>
+        <div className="card boxShadow">Card2</div>
+        <div className="cardImage boxShadow">
+          <img
+            src="https://image.shutterstock.com/image-vector/powerful-sunscreen-product-banner-water-260nw-1502049644.jpg"
+            alt=""
+          />
+          <div>
+            <h2 style={{ margin: 0 }}>Product </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
